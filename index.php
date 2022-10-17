e72f4227577b41d5267ecd218ef1f65d31f7aefa
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-10-17 19:42:48 +0100

    Updated sass

--- index.php
+++ index.php
@@ -1,17 +1,13 @@
-<?php 
-$mode = "dark";
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./sass/main.css">
     <title>Review</title>
 </head>
-<body <?php if ($mode === 'dark'): ?>class="dark"<?php endif ?>>
+<body>
     <?php
      include('./view/all_header.php');
     ?> 
