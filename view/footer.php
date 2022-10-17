e2fddc22dc33fa0ecc87c59e81098d8370aa7925
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-10-17 23:33:57 +0100

    Updated sass

--- view/footer.php
+++ view/footer.php
@@ -1,3 +1,3 @@
 <footer>
-    Coded by <a href="https://www.lunasmithart.com/" target="_blank" rel="noreferrer">Luna Smith</a>, open-source on <a href="https://github.com/Lu-Smith/php_review_project" target="_blank" rel="noreferrer">GitHub</a>.
+    Coded by <a href="https://www.lunasmithart.com/" target="_blank" rel="noreferrer"> Luna Smith</a>, open-source on <a href="https://github.com/Lu-Smith/php_review_project" target="_blank" rel="noreferrer"> GitHub</a>.
 </footer>
--- view/main/user_main.php
+++ view/main/user_main.php
@@ -2,6 +2,6 @@
    Welcome to my world
 </h2>
 <div class='cards'>
-  <div class="card-front">front</div>
-  <div class="card-front">back</div>
+       <div class="card-front">front</div>
+       <div class="card-front">back</div>
 </div>
