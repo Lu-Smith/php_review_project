88bbb0451cb0752425dc026b45f4f3f62f02fadc
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-10-17 23:39:20 +0100

    Updated sass

--- view/main/user_main.php
+++ view/main/user_main.php
@@ -1,7 +1,13 @@
 <h2>
    Welcome to my world
 </h2>
-<div class='cards'>
+<div class="container">
+   <div class='cards'>
        <div class="card-front">front</div>
        <div class="card-front">back</div>
+   </div>
+   <div class='cards'>
+       <div class="card-front">front</div>
+       <div class="card-front">back</div>
+   </div>
 </div>
