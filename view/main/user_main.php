85eb7d2daece8faf0c95450f7ba994f23e3a92f6
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-10-19 13:13:19 +0100

    Updated sass

--- view/main/user_main.php
+++ view/main/user_main.php
@@ -1,15 +1,17 @@
 <main>
     <h2>
-      Welcome to my world
+      Click on the image
     </h2>
   <div class="container">
-   <div class='cards'>
-     <img src="./view/images/Art 2.1.jpg" alt="front" class="card-front"></img>
-     <img src="./view/images/Art 2.2.jpg" alt="back" class="card-front"></img>
-   </div>
-   <div class='cards'>
-     <img src="./view/images/Art 1.1.jpg" alt="front" class="card-front"></img>
-     <img src="./view/images/Art 5.jpg" alt="back" class="card-front"></img>
-   </div>
+    <div class='cards' tabindex='0'>
+    <img src="./view/images/Art 2.2.jpg" alt="back" class="card-back first-img"></img>
+      <img src="./view/images/Art 2.1.jpg" alt="front" class="card-front first-img"></img>
+      
+    </div>
+    <div class='cards'>
+    <img src="./view/images/Art 1.1.jpg" alt="back" class="card-back second-img"></img>
+      <img src="./view/images/Art 5.jpg" alt="front" class="card-front second-img"></img>
+      
+    </div>
    </div>
 </main>
