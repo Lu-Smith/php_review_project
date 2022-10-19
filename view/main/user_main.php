efd0e264460af161726185e7b54a1ba4aaa556b6
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-10-19 11:41:09 +0100

    Updated user_main.php

--- view/main/user_main.php
+++ view/main/user_main.php
@@ -3,11 +3,7 @@
 </h2>
 <div class="container">
    <div class='cards'>
-       <img src="./view/images/Art 2.1.jpg" alt="" class="card-front">front</img>
-       <img src="./view/images/Art 2.2.jpg" alt="" class="card-front">back</img>
-   </div>
-   <div class='cards'>
-       <img src="./view/images/Art 5.jpg" alt="" class="card-front">front</img>
-       <img src="./view/images/Art 1.1.jpg" alt="" class="card-front">back</img>
+       <img src="./view/images/Art 2.1.jpg" alt="" class="card-front"></img>
+       <img src="./view/images/Art 2.2.jpg" alt="" class="card-front"></img>
    </div>
 </div>
