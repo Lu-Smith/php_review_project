c636da4497035de0fbbc410d116aa8020c884822
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-10-22 23:04:27 +0100

    Updated sass

--- view/main/user_main.php
+++ view/main/user_main.php
@@ -11,8 +11,6 @@
       <img src="./view/images/Art 1.1.jpg" alt="back" class="card-back second-img"></img>
       <img src="./view/images/Art 1.2.jpg" alt="front" class="card-front second-img"></img>
     </div>
-   </div>
-   <div class="container">
     <div class='cards' >
       <img src="./view/images/Art 3.2.jpg" alt="back" class="card-back first-img"></img>
       <img src="./view/images/Art 3.1.jpg" alt="front" class="card-front first-img"></img>
