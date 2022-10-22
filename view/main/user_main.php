89ef4273a07b134611d9206ed47aa8d2db8906c3
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-10-22 22:49:22 +0100

    Updated user_main.php

--- view/main/user_main.php
+++ view/main/user_main.php
@@ -4,14 +4,20 @@
     </h2>
   <div class="container">
     <div class='cards' tabindex='0'>
-    <img src="./view/images/Art 2.2.jpg" alt="back" class="card-back first-img"></img>
+      <img src="./view/images/Art 2.2.jpg" alt="back" class="card-back first-img"></img>
       <img src="./view/images/Art 2.1.jpg" alt="front" class="card-front first-img"></img>
-      
     </div>
     <div class='cards'>
-    <img src="./view/images/Art 1.1.jpg" alt="back" class="card-back second-img"></img>
-      <img src="./view/images/Art 5.jpg" alt="front" class="card-front second-img"></img>
-      
+      <img src="./view/images/Art 1.1.jpg" alt="back" class="card-back second-img"></img>
+      <img src="./view/images/Art 1.2.jpg" alt="front" class="card-front second-img"></img>
+    </div>
+    <div class='cards' tabindex='0'>
+      <img src="./view/images/Art 3.2.jpg" alt="back" class="card-back first-img"></img>
+      <img src="./view/images/Art 3.1.jpg" alt="front" class="card-front first-img"></img>
+    </div>
+    <div class='cards'>
+      <img src="./view/images/Art 4.1.jpg" alt="back" class="card-back second-img"></img>
+      <img src="./view/images/Art 4.2.jpg" alt="front" class="card-front second-img"></img>
     </div>
    </div>
 </main>
